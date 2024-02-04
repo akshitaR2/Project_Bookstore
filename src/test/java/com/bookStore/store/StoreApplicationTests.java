b7722b9a6931ecf4a3e961e409a02a918f12d309
akshita rawat <akshi.rawat2@gmail.com>
2024-02-04 15:39:07 +0000

    setting environment

--- src/test/java/com/bookStore/store/StoreApplicationTests.java
+++ src/test/java/com/bookStore/store/StoreApplicationTests.java
@@ -1,13 +1,13 @@
-package com.bookStore.store;
+// package com.bookStore.store;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class StoreApplicationTests {
+// @SpringBootTest
+// class StoreApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }
